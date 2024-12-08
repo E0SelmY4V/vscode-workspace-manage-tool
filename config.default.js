854a4360b4cadf91ff2ca13dd68b2ce2c16c204f
E0SelmY4V <yabo1887415157@qq.com>
2024-12-09 01:39:46 +0800

    ref: 不再跟踪我自己的配置

---
+++ config.default.js
@@ -0,0 +1,36 @@
+/**
+ * @typedef {{
+ *   name: string;
+ *   icon?: string;
+ * } & (
+ *   | { file: string; }
+ *   | { file?: string; onclick(): void; }
+ * )} WorkspaceInfo
+ */
+/**
+ * @typedef {[
+ *   { name: string; path: string; },
+ *   ...(WorkspaceInfo | WorkspaceInfoList)[],
+ * ]} WorkspaceInfoList
+ */
+/**
+ * @typedef {{
+ *   code: string;
+ *   bgp: string;
+ *   maxBgp: number;
+ *   workspace: (WorkspaceInfo | WorkspaceInfoList)[];
+ *   sidebar: {
+ *     name: string;
+ *     action(): void;
+ *   }[];
+ * }} Config
+ */
+
+/**@type {Config} */
+var CONFIG = {
+	code: "E:\\VSCode\\Code.exe",
+	bgp: "https://source.unsplash.com/1920x1080/?snow,nature",
+	maxBgp: 10,
+	workspace: [],
+	sidebar: []
+};
