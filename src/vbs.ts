46bc1e1af36fc10616a9640c779ee8baf7c3aa5e
E0SelmY4V <yabo1887415157@qq.com>
2025-12-11 09:16:35 +0800

    fix: 错误的函数签名导致我无法打开窗口

--- src/vbs.ts
+++ src/vbs.ts
@@ -3,7 +3,7 @@ declare global {
 	function reqBgp(No: number): void;
 	function runcode(code: string, file: string, admin: boolean): void;
 	function runvim(d: string, folder: string): void;
-	function runfile(file: string, show: boolean, sync: boolean): void;
+	function runfile(file: string, show: 0 | 1 | 2 | 3, sync: boolean): void;
 	function exec(command: string): string;
 }
 
